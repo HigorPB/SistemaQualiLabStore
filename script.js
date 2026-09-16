294916eae5245ddd43e65be14e08c07461874703
lucas da cunha santos <lcunha558@gmail.com>
2026-09-15 23:15:18 -0300

    fix: corrige reset do carrinho ao inserir 4 caracteres na busca

--- script.js
+++ script.js
@@ -181,11 +181,6 @@ function buscarProduto() {
   var termo = document.getElementById("busca").value.toLowerCase();
   ultimaBusca = termo;
 
-  if (termo.length > 4) {
-    carrinho = [];
-    atualizarCarrinho();
-  }
-
   var filtrados = produtos.filter(function (p) {
     return p.nome.toLowerCase().includes(termo);
   });
